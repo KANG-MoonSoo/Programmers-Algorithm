7e4e73c28bc4a1cbbe8dac611da5280242212fc2
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-05-17 23:52:09 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int n;
+        if (direction.equals("right")){
+            n = numbers[numbers.length - 1];
+            for (int i = numbers.length - 2; i >= 0 ; i--) {
+                numbers[i+1] = numbers[i];
+            }
+            numbers[0] = n;
+        }
+        else {
+            n = numbers[0];
+            for (int i = 1; i < numbers.length; i++) {
+                numbers[i-1] = numbers[i];
+            }
+            numbers[numbers.length-1] = n;
+        }
+        return numbers;
+    }
+}
