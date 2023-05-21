3d6b1089b631a1aad7a1777b749b3104a42835ab
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-05-21 16:11:47 +0900

    [level 0] Title: 중복된 문자 제거, Time: 13.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String str) {
+        String ans = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i){
+                ans += str.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
