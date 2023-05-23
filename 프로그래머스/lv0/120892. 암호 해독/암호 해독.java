bf0e0e066516aaf6e7d132453a2461acccb4b223
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-05-23 22:21:04 +0900

    [level 0] Title: 암호 해독, Time: 9.34 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String ans = "";
+        for (int i = code - 1; i < cipher.length(); i += code) {
+            ans += cipher.charAt(i);
+        }
+        return ans;
+    }
+}
