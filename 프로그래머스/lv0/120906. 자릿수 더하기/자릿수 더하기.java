5eb19437bb44582b96f8277a77d1e997740f56a2
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-06-05 15:54:07 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        String str = Integer.toString(n);
+        for (int i = 0; i < str.length(); i++) {
+            ans += str.charAt(i) - '0';
+        }
+        return ans;
+    }
+}
