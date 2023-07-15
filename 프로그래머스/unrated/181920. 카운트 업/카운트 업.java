5ae2f4b15c936829fccf5a3cddd54cf9a599ef0b
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-07-15 17:19:43 +0900

    [unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] arr = new int[end - start + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = start;
+            start++;
+        }
+        return arr;
+    }
+}
