5420711cf6f9f0116d25b7492b4b996b4743c6d3
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-07-20 04:03:28 +0900

    [unrated] Title: 접미사 배열, Time: 0.17 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        String[] ans = new String[my_string.length()];
+        for (int i = 0; i < my_string.length(); i++) {
+            ans[i] = my_string.substring(i, my_string.length());
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}
