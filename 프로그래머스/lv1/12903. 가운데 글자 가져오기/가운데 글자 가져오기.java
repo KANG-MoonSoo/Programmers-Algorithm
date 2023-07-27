b6e5b0d176911b1839635e06ba693f24721388f5
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2023-07-27 20:23:12 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 11.81 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String ans = "";
+        if (s.length() % 2 == 0){
+            ans += s.charAt(s.length() / 2 - 1);
+            ans += s.charAt(s.length() / 2 );
+        }
+        else {
+            ans += s.charAt(s.length() / 2);
+        }
+        return ans;
+    }
+}
